a287b833cdb38befd944d391aaef9247546ec6ea
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-01-07 13:03:48 +0530

    created a counter using js

---
+++ Practice/js/counter/counter.js
@@ -0,0 +1,20 @@
+const incrementBtn = document.querySelector("#increment");
+
+const decrementBtn = document.querySelector("#decrement");
+const counterEl = document.getElementById("counter");
+
+let counter = 0;
+function incrementCounter() {
+  counter++;
+  counterEl.innerText = counter;
+  console.log(counter);
+}
+
+function decrementCounter() {
+  counter--;
+  counterEl.innerText = counter;
+  console.log(counter);
+}
+
+incrementBtn.addEventListener("click", incrementCounter);
+decrementBtn.addEventListener("click", decrementCounter);
