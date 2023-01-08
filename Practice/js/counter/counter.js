2dcfb9e6a84498e98bc9d954b9a414e06ed41383
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-01-08 11:11:35 +0530

    adding and removing elements using dom

--- Practice/js/counter/counter.js
+++ Practice/js/counter/counter.js
@@ -2,18 +2,30 @@ const incrementBtn = document.querySelector("#increment");
 
 const decrementBtn = document.querySelector("#decrement");
 const counterEl = document.getElementById("counter");
+const ulElement = document.getElementById("list-items");
 
 let counter = 0;
 function incrementCounter() {
   counter++;
   counterEl.innerText = counter;
-  console.log(counter);
+
+  // create an element
+  const li = document.createElement("li");
+  li.setAttribute("data-counter", counter);
+  li.innerHTML = "<b>Sentence </b> " + counter;
+  console.log(li);
+
+  // append tht element
+  ulElement.appendChild(li);
+  // console.log(counter);
 }
 
 function decrementCounter() {
+  const li = ulElement.querySelector('[data-counter="' + counter + '"]');
+  li.remove();
   counter--;
   counterEl.innerText = counter;
-  console.log(counter);
+  // console.log(counter);
 }
 
 incrementBtn.addEventListener("click", incrementCounter);
