1007b66009f981b4701cac9bf3d2b8d707fa0632
Shubhamdeulkar06 <shuboy0868@gmail.com>
2023-03-23 12:12:13 +0530

    made changes in design

--- React/React-Projects/meal-finder-app/src/components/Cards.jsx
+++ React/React-Projects/meal-finder-app/src/components/Cards.jsx
@@ -14,7 +14,9 @@ export const Cards = (getMeal) => {
         </p>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <span className="inline-block bg-gray-200 dark:bg-gray-800 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2"></span>
+        <span className="inline-block bg-gray-200 dark:bg-gray-800 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 dark:text-gray-200 mr-2 mb-2">
+          {getMeal.data.strTags}
+        </span>
       </div>
     </div>
   );
--- React/React-Projects/meal-finder-app/src/components/Meal.jsx
+++ React/React-Projects/meal-finder-app/src/components/Meal.jsx
@@ -5,29 +5,37 @@ import { Cards } from "./Cards";
 const Meal = () => {
   const [search, setSearch] = useState("");
   const [myMeal, setMyMeal] = useState();
+  const [check, setCheck] = useState(false);
+
   const searchMeal = () => {
     console.log("helooo");
     fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setMyMeal(data.meals);
-        setSearch("");
+        if (data.meals == null) {
+          alert("Food Not Found");
+          setSearch("");
+        } else {
+          setMyMeal(data.meals);
+          setSearch("");
+          setCheck(true);
+        }
       });
   };
   return (
-    <div className="main ">
+    <div className="main px-2">
       <div className="heading">
         <h1 className="text-white text-4xl font-extrabold pt-8 text-center">
           Search Your Food Here
         </h1>
       </div>
-      <div className="searchBox text-center m-12">
+      <div className="searchBox flex gap-1 justify-center items-center m-6 md:m-12">
         <input
           type="text"
           name=""
           id=""
-          className="search-bar w-80 h-10 outline-none border-none py-1 px-4 rounded-3xl mr-2"
+          className="search-bar w-[70%] h-10 outline-none border-none py-1 px-4 rounded-3xl mr-2 md:w-80"
           placeholder="What do you want?........"
           onChange={(e) => setSearch(e.target.value)}
           value={search}
@@ -41,13 +49,12 @@ const Meal = () => {
       </div>
       <div className="container w-[80%] my-0 mx-auto flex justify-around flex-wrap">
         <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-5">
-          {/* <Mealitems /> */}
-          {myMeal == null ? (
-            <p className="text-white">Not Found</p>
-          ) : (
+          {myMeal ? (
             myMeal.map((res) => {
-              return <Cards data={res} />;
+              return <Cards data={res} key={res.idMeal} />;
             })
+          ) : (
+            <p className="text-white text-center "></p>
           )}
         </div>
       </div>
